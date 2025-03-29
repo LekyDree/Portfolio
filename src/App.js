5e7da183929fa1377228ddc74a9e53d407c0296f
LekyDree <kreed091803@gmail.com>
2025-03-28 23:55:14 -0400

    Began work on home page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 function Home() {
   useEffect(() => {
@@ -6,8 +8,20 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="homepage">
+      <h1 className="title-top">Explore Exotic Wonders in</h1>
+      <h1 className="title-bottom">Kyle Reed's Digital Jungle</h1>
+      <div className="button-container">
+        <Link to="/projects" className="button">
+          Explore
+        </Link>
+        <Link to="/about" className="button">
+          Meet The Founder
+        </Link>
+        <Link to="/contact" className="button">
+          Book a Ticket
+        </Link>
+      </div>
     </div>
   );
 }
