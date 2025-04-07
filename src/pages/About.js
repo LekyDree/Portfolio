7f2683c503d6c3d6413f60cf2f01869cdcbc613c
LekyDree <kreed091803@gmail.com>
2025-04-07 19:54:37 -0400

    Temporarily removed Sign Model to test if it the project can build.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import SignModel from "../components/SignModel";
 import "../styles/About.css";
 
 function About() {
@@ -13,11 +11,7 @@ function About() {
   return (
     <div className="about-page">
       <Header />
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[9, 6, 7]} />
-        <SignModel />
-      </Canvas>
+
       <Footer />
     </div>
   );
