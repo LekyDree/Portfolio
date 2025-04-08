3485863c3d7420f6b26f908520a012bdb559b41e
LekyDree <kreed091803@gmail.com>
2025-04-08 00:10:05 -0400

    Readded sign using CSS

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,55 @@
-import { useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/About.css";
-
-import { Suspense } from "react";
 
 function About() {
   useEffect(() => {
     document.title = "About Me";
   }, []);
 
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div className="about-page">
       <Header />
-      <Suspense fallback={null}>
-        <Canvas>
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[9, 6, 7]} />
-        </Canvas>
-      </Suspense>
+
+      <div
+        className={`cube-container ${clicked ? "clicked" : ""}`}
+        style={
+          clicked
+            ? {
+                transform: `translate(-50%, -42%) scale(${
+                  window.innerWidth > 650
+                    ? (window.innerHeight * 0.8) / 350
+                    : window.innerWidth / 250
+                })`,
+              }
+            : {}
+        }
+        onClick={() => setClicked(!clicked)}
+      >
+        <div className={`cube ${clicked ? "clicked" : ""}`}>
+          <div className="face front"></div>
+          <div className="face back"></div>
+          <div className="face left"></div>
+          <div className="face right"></div>
+          <div className="face top"></div>
+          <div className="face bottom"></div>
+
+          <div class="face inset inset-top"></div>
+          <div class="face inset inset-bottom"></div>
+          <div class="face inset inset-left"></div>
+          <div class="face inset inset-right"></div>
+          <div class="face inset inset-back"></div>
+
+          <div class="face frame-top"></div>
+          <div class="face frame-bottom"></div>
+          <div class="face frame-left"></div>
+          <div class="face frame-right"></div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
