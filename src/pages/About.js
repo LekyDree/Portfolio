15bb9644f8545ebd4aaf777bc46be49295280f7d
LekyDree <kreed091803@gmail.com>
2025-04-07 20:19:32 -0400

    Trying to lazy load the Sign model

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/About.css";
+
+import { Suspense, lazy } from "react";
+const SignModel = lazy(() => import("../components/SignModel"));
 
 function About() {
   useEffect(() => {
@@ -11,7 +15,13 @@ function About() {
   return (
     <div className="about-page">
       <Header />
-
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[9, 6, 7]} />
+        <Suspense fallback={null}>
+          <SignModel />
+        </Suspense>
+      </Canvas>
       <Footer />
     </div>
   );
